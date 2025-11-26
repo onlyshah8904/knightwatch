25272abf587a9030104984214a45fcd073315f1c
onlyshah8904 <kevalcontrol@gmail.com>
2025-11-27 01:02:24 +0530

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,7 +1,7 @@
-TOKEN = "MTM1MjYzNTg2MTc1NzU5MTY1NQ.GZiN_t.P4pUqTeNs633gDTVIa_nXCGDiLu0le5oQNePRY"  # Replace with your new token
-CHANNEL_ID = 1352605882680348826
+TOKEN = ""  # Replace with your new token
+CHANNEL_ID = ""
 DB_CONFIG = {
-    "host": "15.235.85.189",
+    "host": "",
     "user": "root",
     "password": "actowiz"
 }
